ffd4da80241d04444d97aae8500ac87b4b843742
Ben Wibking <ben@wibking.com>
2025-05-23 18:02:48 -0400

    update linux launcher scripts

--- launch_amrvis3d_linux.sh
+++ launch_amrvis3d_linux.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 ## Wrapper script to launch amrvis2d inside a Docker container on Linux
+DISPLAY=:0.0
+xhost +local:docker
 
-PORT=1
+PORT=0
 MOUNT_DIR=${1:-`pwd`}
 AMRVIS_EXE=/Amrvis3D/amrvis3d.gnu.ex
 
-docker run --platform linux/amd64 -v $MOUNT_DIR:/home/vscode/data -e DISPLAY=localhost:$PORT ghcr.io/benwibking/amrvis-container:main $AMRVIS_EXE
+docker run -it --platform linux/amd64 -v $MOUNT_DIR:/home/vscode/data -e DISPLAY=localhost:$PORT ghcr.io/benwibking/amrvis-container:main $AMRVIS_EXE
