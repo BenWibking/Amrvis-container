f735fc45866f2fedb8d257339a418852d9520286
Ben Wibking <wibkingb@msu.edu>
2025-05-23 18:22:02 -0400

    update linux launcher scripts to work on Frontier

--- launch_amrvis3d_linux.sh
+++ launch_amrvis3d_linux.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 ## Wrapper script to launch amrvis2d inside a Docker container on Linux
-DISPLAY=:0.0
-xhost +local:docker
-
-PORT=0
 MOUNT_DIR=${1:-`pwd`}
 AMRVIS_EXE=/Amrvis3D/amrvis3d.gnu.ex
+DOCKER_CMD=singularity
 
-docker run -it --platform linux/amd64 -v $MOUNT_DIR:/home/vscode/data -e DISPLAY=localhost:$PORT ghcr.io/benwibking/amrvis-container:main $AMRVIS_EXE
+$DOCKER_CMD run --env DISPLAY=$DISPLAY docker://ghcr.io/benwibking/amrvis-container:main $AMRVIS_EXE
